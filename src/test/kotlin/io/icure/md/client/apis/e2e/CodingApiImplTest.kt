7d2b5ca4e621fe31746c15a46fab264ec7032cc2
Clément Vandendaelen <cv@taktik.com>
2022-02-04 18:37:26 +0100

    Added coding test

---
+++ src/test/kotlin/io/icure/md/client/apis/e2e/CodingApiImplTest.kt
@@ -0,0 +1,69 @@
+package io.icure.md.client.apis.e2e
+
+import io.icure.kraken.client.infrastructure.Diff
+import io.icure.kraken.client.infrastructure.differences
+import io.icure.kraken.client.infrastructure.filterDiffs
+import io.icure.md.client.apis.CodingApi
+import io.icure.md.client.apis.MedTechApi
+import io.icure.md.client.apis.impl.CodingApiImpl
+import io.icure.md.client.models.Coding
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import java.util.*
+
+@ExperimentalStdlibApi
+@ExperimentalCoroutinesApi
+@DisplayName("Coding tests")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+internal class CodingApiImplTest {
+
+    private val iCurePath = "https://kraken.icure.dev"
+    private val authHeader = TestUtils.basicAuthFrom(".credentials")
+    private val healthcareProfessionalId = "782f1bcd-9f3f-408a-af1b-cd9f3f908a98"
+    private val healthcareProfessionalPrivateKey =
+        TestUtils.healthcareProfessionalPrivateKey(healthcareProfessionalId, this::class.java)
+    private val healthcareProfessionalPublicKey =
+        runBlocking { TestUtils.healthcareProfessionalPublicKey(iCurePath, authHeader, healthcareProfessionalId) }
+
+    private val medTechApi = MedTechApi.Builder()
+        .iCureUrlPath(iCurePath)
+        .authorization(authHeader)
+        .addKeyPair(healthcareProfessionalId, healthcareProfessionalPublicKey, healthcareProfessionalPrivateKey)
+        .build()
+
+    private val testedInstance: CodingApi = CodingApiImpl(medTechApi)
+
+    @Test
+    @DisplayName("Create coding test - HappyFlow")
+    fun createDeviceHappyFlow() = runBlocking {
+        val coding = coding()
+        val createdCoding = testedInstance.createOrModifyCodings(listOf(coding)).single()
+
+        val diffs = coding.differences(createdCoding)
+        val filters = listOf("id", "rev")
+        val filteredDiffs = filterDiffs(createdCoding, coding, diffs, filters)
+        Assertions.assertEquals(emptyList<Diff>(), filteredDiffs)
+    }
+
+    @Test
+    @DisplayName("Get coding test - HappyFlow")
+    fun getDeviceHappyFlow() = runBlocking {
+        val coding = coding()
+        val createdCoding = testedInstance.createOrModifyCodings(listOf(coding)).single()
+        val gotCoding = testedInstance.getCoding(createdCoding.id!!)
+
+        val diffs = createdCoding.differences(gotCoding)
+        Assertions.assertEquals(emptyList<Diff>(), diffs)
+    }
+
+    private fun coding() = Coding(
+        description = mapOf("fr" to "Bonjour le monde", "en" to "Hello world"),
+        code = "${UUID.randomUUID()}",
+        type = "TEST",
+        version = "1"
+    )
+}
