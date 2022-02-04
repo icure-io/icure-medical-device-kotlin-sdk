3a06eab9ddb75cbe92159f217de7597c07ea535a
lisatr2812 <lisa.trapletti@gmail.com>
2022-02-04 12:40:14 +0100

    Implement Data Sample delete and get

--- src/main/kotlin/io/icure/md/client/apis/impl/DataSampleApiImpl.kt
+++ src/main/kotlin/io/icure/md/client/apis/impl/DataSampleApiImpl.kt
@@ -3,12 +3,18 @@ package io.icure.md.client.apis.impl
 import io.icure.kraken.client.crypto.contactCryptoConfig
 import io.icure.kraken.client.crypto.patientCryptoConfig
 import io.icure.kraken.client.extendedapis.createContact
+import io.icure.kraken.client.extendedapis.deleteServices
+import io.icure.kraken.client.extendedapis.filterContactsBy
 import io.icure.kraken.client.extendedapis.getContact
 import io.icure.kraken.client.extendedapis.getPatient
+import io.icure.kraken.client.extendedapis.listServices
+import io.icure.kraken.client.models.FilterChainContact
+import io.icure.kraken.client.models.ListOfIdsDto
 import io.icure.kraken.client.models.decrypted.ContactDto
+import io.icure.kraken.client.models.filter.contact.ContactByServiceIdsFilter
 import io.icure.md.client.apis.DataSampleApi
 import io.icure.md.client.apis.MedTechApi
-import io.icure.md.client.mappers.findIdForEncryptionKey
+import io.icure.md.client.mappers.findDataOwnerId
 import io.icure.md.client.mappers.toDataSample
 import io.icure.md.client.mappers.toServiceDto
 import io.icure.md.client.models.DataSample
@@ -47,7 +53,7 @@ class DataSampleApiImpl(private val medTechApi: MedTechApi) : DataSampleApi {
         }
 
         val contactPatientId = existingContact?.let {
-            localCrypto.decryptEncryptionKeys(currentUser.findIdForEncryptionKey(), it.cryptedForeignKeys)
+            localCrypto.decryptEncryptionKeys(currentUser.findDataOwnerId(), it.cryptedForeignKeys)
                 .firstOrNull()
         }
 
@@ -106,11 +112,44 @@ class DataSampleApiImpl(private val medTechApi: MedTechApi) : DataSampleApi {
     }
 
     override suspend fun deleteDataSample(dataSampleId: String): String {
-        TODO("Not yet implemented")
+        return deleteDataSamples(listOf(dataSampleId)).firstOrNull()
+            ?: throw RuntimeException("Couldn't delete data sample $dataSampleId")
     }
 
     override suspend fun deleteDataSamples(dataSampleIds: List<String>): List<String> {
-        TODO("Not yet implemented")
+        val localCrypto = medTechApi.localCrypto()
+        val contactApi = medTechApi.contactApi()
+        val currentUser = medTechApi.userApi().getCurrentUser()
+
+        val existingContacts = contactApi
+            .filterContactsBy(
+                currentUser, FilterChainContact(ContactByServiceIdsFilter(ids = dataSampleIds)), null, null,
+                dataSampleIds.size, null, null, null, contactCryptoConfig(localCrypto, currentUser)
+            )
+            .rows
+
+        if (existingContacts.size > 1) {
+            throw IllegalArgumentException("Only data samples of a same batch can be processed together")
+        }
+
+        val existingContact = existingContacts.first()
+        val contactPatient =
+            localCrypto.decryptEncryptionKeys(currentUser.findDataOwnerId(), existingContact.cryptedForeignKeys)
+                .first()
+                .let { medTechApi.patientApi().getPatient(currentUser, it, patientCryptoConfig(localCrypto)) }
+
+        val servicesToDelete = existingContact.services.filter { it.id in dataSampleIds }
+
+        return medTechApi.contactApi().deleteServices(
+            currentUser,
+            contactPatient,
+            servicesToDelete,
+            contactCryptoConfig(localCrypto, currentUser)
+        )
+            .services
+            .filter { it.id in dataSampleIds }
+            .filter { it.endOfLife != null }
+            .map { it.id }
     }
 
     override suspend fun filterDataSample(filter: Filter): PaginatedListDataSample {
@@ -118,7 +157,17 @@ class DataSampleApiImpl(private val medTechApi: MedTechApi) : DataSampleApi {
     }
 
     override suspend fun getDataSample(dataSampleId: String): DataSample {
-        TODO("Not yet implemented")
+        val localCrypto = medTechApi.localCrypto()
+        val currentUser = medTechApi.userApi().getCurrentUser()
+
+        return medTechApi.contactApi().listServices(
+            currentUser,
+            ListOfIdsDto(listOf(dataSampleId)),
+            contactCryptoConfig(localCrypto, currentUser).crypto
+        )
+            .firstOrNull()
+            ?.toDataSample()
+            ?: throw IllegalArgumentException("Id $dataSampleId does not correspond to any existing data sample")
     }
 
     override suspend fun getDataSampleAttachment(dataSampleId: String): Document {
