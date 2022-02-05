53b482d58dec3e6af5c586d2e0ea392042b79a21
Antoine Duchâteau <ad@taktik.com>
2022-02-05 12:46:34 +0100

    Add test for DSL

--- src/test/kotlin/io/icure/md/client/filter/unit/FilterTest.kt
+++ src/test/kotlin/io/icure/md/client/filter/unit/FilterTest.kt
@@ -41,4 +41,32 @@ internal class FilterTest {
         Assertions.assertEquals(PatientByHealthcarePartyAndIdentifiersFilter::class, filter.filters[0]::class, "First filter in union must be of class PatientByHealthcarePartyAndIdentifiersFilter")
         Assertions.assertEquals("123", (filter.filters[0] as PatientByHealthcarePartyAndIdentifiersFilter).healthcarePartyId, "Hcp must be set in filters")
     }
+
+    @Test
+    internal fun dslForHcpOrderTest() {
+        val filter1 = filter<Patient> {
+            union {
+                byIdentifiers( Identifier("pat-1"), Identifier("pat-2") )
+                intersection {
+                    byName("Churchill")
+                    byGenderEducation(Patient.Gender.male, "college")
+                }
+            }
+            forHcp(HealthcareProfessional(id = "123"))
+        }.build()
+
+        val filter2 = filter<Patient> {
+            forHcp(HealthcareProfessional(id = "123"))
+            union {
+                byIdentifiers( Identifier("pat-1"), Identifier("pat-2") )
+                intersection {
+                    byName("Churchill")
+                    byGenderEducation(Patient.Gender.male, "college")
+                }
+            }
+        }.build()
+
+        Assertions.assertEquals(filter1, filter2, "Generated filters should be the same")
+    }
+
 }
