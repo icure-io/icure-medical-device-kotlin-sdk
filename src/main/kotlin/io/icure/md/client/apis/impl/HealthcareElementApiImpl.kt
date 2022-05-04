bb365fe911184201ab80dca82ee3e9093b688da2
Clément Vandendaelen <cv@icure.com>
2022-05-04 13:23:34 +0200

    IMKS-14 #time 20m Implemented healthcareElement giveAccessTo
    Signed-off-by: Clément Vandendaelen <cv@taktik.com>

--- src/main/kotlin/io/icure/md/client/apis/impl/HealthcareElementApiImpl.kt
+++ src/main/kotlin/io/icure/md/client/apis/impl/HealthcareElementApiImpl.kt
@@ -9,12 +9,14 @@ import io.icure.kraken.client.extendedapis.getHealthElement
 import io.icure.kraken.client.extendedapis.getPatient
 import io.icure.kraken.client.extendedapis.modifyHealthElement
 import io.icure.kraken.client.extendedapis.modifyHealthElements
+import io.icure.kraken.client.models.DelegationDto
 import io.icure.kraken.client.models.ListOfIdsDto
 import io.icure.kraken.client.models.filter.chain.FilterChain
 import io.icure.md.client.apis.HealthcareElementApi
 import io.icure.md.client.apis.MedTechApi
 import io.icure.md.client.filter.Filter
 import io.icure.md.client.isUUID
+import io.icure.md.client.mappers.dataOwnerId
 import io.icure.md.client.mappers.toAbstractFilterDto
 import io.icure.md.client.mappers.toHealthcareElement
 import io.icure.md.client.mappers.toHealthcareElementDto
@@ -114,5 +116,69 @@ class HealthcareElementApiImpl(private val medTechApi: MedTechApi) : HealthcareE
 
     override suspend fun matchHealthcareElement(filter: Filter<HealthcareElement>): List<String> {
         return medTechApi.baseHealthElementApi.matchHealthElementsBy(filter.toAbstractFilterDto())
+    }
+
+    override suspend fun giveAccessTo(healthcareElement: HealthcareElement, delegateTo: String): HealthcareElement {
+        val localCrypto = medTechApi.localCrypto
+        val currentUser = medTechApi.baseUserApi.getCurrentUser()
+        val dataOwnerId = currentUser.dataOwnerId()
+
+        if (!healthcareElement.systemMetaData!!.delegations.keys.any { it == dataOwnerId }) {
+            throw IllegalStateException("DataOwner $dataOwnerId does not have the right to access it ${healthcareElement.id}")
+        }
+
+        if (healthcareElement.systemMetaData.delegations.keys.any { it == dataOwnerId }) {
+            return healthcareElement
+        }
+
+        val ccHealthElement = healthElementCryptoConfig(localCrypto)
+
+        return healthcareElement.toHealthcareElementDto().let {
+            val (patientIdKey, _) = localCrypto.encryptAESKeyForDataOwner(
+                dataOwnerId,
+                delegateTo,
+                it.id,
+                localCrypto.decryptEncryptionKeys(dataOwnerId, it.cryptedForeignKeys).first()
+            )
+            val (secretForeignKey, _) = localCrypto.encryptAESKeyForDataOwner(
+                dataOwnerId,
+                delegateTo,
+                it.id,
+                localCrypto.decryptEncryptionKeys(dataOwnerId, it.delegations).first()
+            )
+            val (encryptionKey, _) = localCrypto.encryptAESKeyForDataOwner(
+                dataOwnerId,
+                delegateTo,
+                it.id,
+                localCrypto.decryptEncryptionKeys(dataOwnerId, it.encryptionKeys).first()
+            )
+
+            val delegation = DelegationDto(owner = dataOwnerId, delegatedTo = delegateTo, key = secretForeignKey)
+            val encryptionKeyDelegation =
+                DelegationDto(owner = dataOwnerId, delegatedTo = delegateTo, key = encryptionKey)
+            val cryptedForeignKeyDelegation =
+                DelegationDto(owner = dataOwnerId, delegatedTo = delegateTo, key = patientIdKey)
+
+            val delegations = it.delegations.plus(delegateTo to setOf(delegation))
+            val encryptionKeys = it.encryptionKeys.plus(delegateTo to setOf(encryptionKeyDelegation))
+            val cryptedForeignKeys = it.cryptedForeignKeys.plus(delegateTo to setOf(cryptedForeignKeyDelegation))
+
+            val healthElementDtoToUpdate = it.copy(
+                delegations = delegations,
+                encryptionKeys = encryptionKeys,
+                cryptedForeignKeys = cryptedForeignKeys
+            )
+
+            try {
+                medTechApi.baseHealthElementApi.modifyHealthElement(
+                    currentUser,
+                    healthElementDtoToUpdate,
+                    ccHealthElement
+                ).toHealthcareElement()
+            } catch (e: Exception) {
+                e.printStackTrace()
+                throw IllegalStateException("Couldn't give access to dataSample")
+            }
+        }
     }
 }
