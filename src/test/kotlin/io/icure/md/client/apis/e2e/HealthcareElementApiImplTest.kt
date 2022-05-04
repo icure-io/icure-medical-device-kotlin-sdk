34eb57b6a85c59abbd93f0b5a632ea66c5d767d1
Clément Vandendaelen <cv@taktik.com>
2022-05-04 16:10:25 +0200

    IMKS-14 #time 30m Added test for HealthcareElement giveAccessTo
    Signed-off-by: Clément Vandendaelen <cv@taktik.com>

--- src/test/kotlin/io/icure/md/client/apis/e2e/HealthcareElementApiImplTest.kt
+++ src/test/kotlin/io/icure/md/client/apis/e2e/HealthcareElementApiImplTest.kt
@@ -3,25 +3,14 @@ package io.icure.md.client.apis.e2e
 import io.icure.diffutils.Diff
 import io.icure.diffutils.differences
 import io.icure.diffutils.filterDiffs
-import io.icure.kraken.client.crypto.patientCryptoConfig
-import io.icure.kraken.client.extendedapis.createPatient
-import io.icure.md.client.apis.HealthcareElementApi
-import io.icure.md.client.apis.MedTechApi
-import io.icure.md.client.apis.impl.HealthcareElementApiImpl
-import io.icure.md.client.mappers.toPatient
-import io.icure.md.client.mappers.toPatientDto
 import io.icure.md.client.models.HealthcareElement
-import io.icure.md.client.models.Patient
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.single
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
-import java.util.*
 import kotlin.time.ExperimentalTime
 
 @FlowPreview
@@ -32,40 +21,14 @@ import kotlin.time.ExperimentalTime
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class HealthcareElementApiImplTest {
 
-    private val iCurePath = "https://kraken.icure.dev"
-    private val authHeader = TestUtils.basicAuthFrom(".credentials")
-    private val healthcareProfessionalId = "782f1bcd-9f3f-408a-af1b-cd9f3f908a98"
-    private val healthcareProfessionalPrivateKey =
-        TestUtils.healthcareProfessionalPrivateKey(healthcareProfessionalId, this::class.java)
-    private val healthcareProfessionalPublicKey =
-        runBlocking { TestUtils.healthcareProfessionalPublicKey(iCurePath, authHeader, healthcareProfessionalId) }
-
-    private val medTechApi = MedTechApi.Builder()
-        .iCureUrlPath(iCurePath)
-        .authorization(authHeader)
-        .addKeyPair(healthcareProfessionalId, healthcareProfessionalPublicKey, healthcareProfessionalPrivateKey)
-        .build()
-
-    private val testedInstance: HealthcareElementApi = HealthcareElementApiImpl(medTechApi)
-    private val patientId = runBlocking {
-        createPatientFlow().single().id ?: throw Exception("Patient should be created before Healthcare Element tests")
-    }
-
-    private fun createPatientFlow() = flow<Patient> {
-        emit(
-            medTechApi.basePatientApi.createPatient(
-                medTechApi.baseUserApi.getCurrentUser(),
-                patient().toPatientDto(),
-                patientCryptoConfig(medTechApi.localCrypto)
-            ).toPatient()
-        )
-    }
-
     @Test
     @DisplayName("Create healthcare element test - HappyFlow")
     fun createHealthcareElementHappyFlow() = runBlocking {
+        val patCred = TestUtils.UserCredentials.fromFile("pat_e810366a-89b6-4cd5-a36a-41e002344e6c.json")
+        val api = patCred.api
+
         val he = healthElement()
-        val createdHe = testedInstance.createOrModifyHealthcareElement(patientId, he)
+        val createdHe = api.healthcareElementApi().createOrModifyHealthcareElement(patCred.dataOwnerId, he)
 
         val diffs = he.differences(createdHe)
         val filters = listOf(
@@ -87,25 +50,42 @@ internal class HealthcareElementApiImplTest {
     @Test
     @DisplayName("Get healthcare element test - HappyFlow")
     fun getHealthcareElementHappyFlow() = runBlocking {
+        val patCred = TestUtils.UserCredentials.fromFile("pat_e810366a-89b6-4cd5-a36a-41e002344e6c.json")
+        val api = patCred.api
+
         val he = healthElement()
-        val createdHe = testedInstance.createOrModifyHealthcareElement(patientId, he)
-        val gotHe = testedInstance.getHealthcareElement(createdHe.id!!)
+        val createdHe = api.healthcareElementApi().createOrModifyHealthcareElement(patCred.dataOwnerId, he)
+        val gotHe = api.healthcareElementApi().getHealthcareElement(createdHe.id!!)
 
         val diffs = createdHe.differences(gotHe)
         Assertions.assertEquals(emptyList<Diff>(), diffs)
     }
 
-    private fun healthElement() = HealthcareElement(
-        note = "This is a test",
-    )
+    @Test
+    @DisplayName("Sharing delegation of DecryptedHealthElementDto patient to HCP")
+    fun shareDelegationOhHealthCareElementFromPatientToHcp() {
+        runBlocking {
+            val patCred = TestUtils.UserCredentials.fromFile("pat_e810366a-89b6-4cd5-a36a-41e002344e6c.json")
+            val hcpCred = TestUtils.UserCredentials.fromFile("hcp_2c5f952e-512b-4fd3-bc6d-0f66c282c159.json")
 
-    private fun patient() = Patient(
-        id = UUID.randomUUID().toString(),
-        firstName = "John",
-        lastName = "Doe",
-        note = "To be encrypted",
-        birthSex = Patient.BirthSex.male,
-        gender = Patient.Gender.male,
-        personalStatus = Patient.PersonalStatus.single
+            val currentPatUser = patCred.api.userApi().getLoggedUser()
+            val currentHcpUser = hcpCred.api.userApi().getLoggedUser()
+
+            val patientFromPat = patCred.api.patientApi()
+                .getPatient(currentPatUser.patientId ?: throw IllegalArgumentException("User must be a Patient"))
+            val createdHEFromPatient =
+                patCred.api.healthcareElementApi().createOrModifyHealthcareElement(patientFromPat.id!!, healthElement())
+            val sharedHE = patCred.api.healthcareElementApi().giveAccessTo(
+                createdHEFromPatient,
+                currentHcpUser.healthcarePartyId ?: throw IllegalArgumentException("User must be a HCP")
+            )
+            val gotHEFromHcp = hcpCred.api.healthcareElementApi().getHealthcareElement(sharedHE.id!!)
+
+            Assertions.assertEquals(sharedHE, gotHEFromHcp)
+        }
+    }
+
+    private fun healthElement() = HealthcareElement(
+        note = "Stay hungry. Stay foolish",
     )
 }
